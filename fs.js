5760f22ed08c5f53629b19579e0010319d11a6f3
saba akram <sabaakram211@gmail.com>
2023-03-26 11:33:30 +0530

    crud of fs

--- fs.js
+++ fs.js
@@ -1,7 +1,13 @@
 let fs = require('fs');
 let path = require('path');
 let newFilePath  = path.join(__dirname,'text.text');
-console.log(newFilePath);
-// fs.writeFileSync(newFilePath,"HI, I am ready");
+// console.log(newFilePath);
+fs.writeFileSync(newFilePath,"HI, I am ready");
 //create file if not exist otherwise It overrides
-fs.writeFileSync(newFilePath,"I am writing again");
+// fs.writeFileSync(newFilePath,"I am writing again");
+let content = fs.readFileSync(newFilePath,'utf-8');
+console.log(content);
+
+fs.appendFileSync(newFilePath," updated now");
+console.log(fs.readFileSync(newFilePath,'utf-8'));
+
