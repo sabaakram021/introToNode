d2f75b284f1463396026d16cb3b0b0d8c9339bd8
saba akram <sabaakram211@gmail.com>
2023-03-28 10:42:38 +0530

    questions

--- fs.js
+++ fs.js
@@ -1,13 +1,36 @@
 let fs = require('fs');
 let path = require('path');
-let newFilePath  = path.join(__dirname,'text.text');
+// let newFilePath  = path.join(__dirname,'text.text');
 // console.log(newFilePath);
-fs.writeFileSync(newFilePath,"HI, I am ready");
+// fs.writeFileSync(newFilePath,"HI, I am ready");
 //create file if not exist otherwise It overrides
 // fs.writeFileSync(newFilePath,"I am writing again");
-let content = fs.readFileSync(newFilePath,'utf-8');
-console.log(content);
+// let content = fs.readFileSync(newFilePath,'utf-8');
+// console.log(content);
 
-fs.appendFileSync(newFilePath," updated now");
-console.log(fs.readFileSync(newFilePath,'utf-8'));
+// fs.appendFileSync(newFilePath," updated now");
+// console.log(fs.readFileSync(newFilePath,'utf-8'));
 
+// let dirctoryPath = path.join(__dirname,'module');
+// // console.log(dirctoryPath);
+// // fs.mkdirSync(dirctoryPath);
+// try {
+//     if (!fs.existsSync(dirctoryPath)) {
+//         fs.mkdirSync(dirctoryPath);
+//     }
+// } catch (err) {
+//     console.error(err);
+// }
+
+// let content = fs.readdirSync(dirctoryPath);
+// console.log(content);
+
+// fs.mkdirSync("delete");
+// fs.rmdirSync('delete');
+let sourcePath = path.join(__dirname,'file.txt');
+console.log(sourcePath);
+
+let destinationPath = path.join(__dirname,'module','file.txt');
+console.log(destinationPath);
+
+fs.copyFileSync(sourcePath,destinationPath);
--- moduleTest.js
+++
@@ -1,4 +0,0 @@
-let calc = require("./calculator.js");
-
-console.log(calc.addition(2,10));
-console.log(calc.multiplication(2,5));
