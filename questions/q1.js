8f877cb2e667b4b213fcae7b2ef674169a8b3af6
saba akram <sabaakram211@gmail.com>
2023-03-31 15:04:08 +0530

    q1 solved

--- questions/q1.js
+++ questions/q1.js
@@ -1,13 +1,15 @@
 //make a folder inside same directory of this file and in that folder make a text file with content
 //new file has been made
-let fs = require("fs");
-let path = require("path");
 
-if(!fs.existsSync("newFolder")){
-    fs.mkdirSync("newFolder");
+let path = require('path');
+let fs = require('fs');
+
+// let dirctoryPath = __dirname;
+// console.log(dirctoryPath);
+if(!fs.existsSync("new_folder")){
+    fs.mkdirSync("new_folder");
 }
+let pathofTextFile = path.join(__dirname,'new_folder','newFile.txt');
+// console.log(pathofTextFile);
+fs.writeFileSync(pathofTextFile,"new file has been made");
 
-let fileKaPath = path.join(__dirname,"newFolder","newFile.txt"); // responsible for path 
-console.log(fileKaPath);
-
-fs.writeFileSync(fileKaPath,"new file has been made"); // responsible create a file with content
--- questions/q2.js
+++ questions/q2.js
@@ -1,24 +1,15 @@
 //Q. given an array arr=[audio,video,image,software,documents,applications,other]
 //   make folder for each element in the given array and inside each folder make 4 files of that type
 
-let fs = require("fs");
-let path = require("path");
-
-let arr = ['Audio','Video','Software','Documents','Applications','Others'];
-let nameArr = ['abc','efg','xyz','def'];
-let extArr = ['.mp3','.mp4','.exe','.pdf','.apk','.rar'];
-
-let organisePath = path.join(__dirname,"Organise");
-if(!fs.existsSync(organisePath))
-    fs.mkdirSync(organisePath);
-
-for(let i=0;i<arr.length;i++){
-    let folderKaPath = path.join(organisePath,arr[i]);
-    if(!fs.existsSync(folderKaPath))
-        fs.mkdirSync(folderKaPath)
-    for(let j=0;j<nameArr.length;j++){
-        let fileName = nameArr[j]+extArr[i];
-        let fileKaPath = path.join(folderKaPath,fileName);
-        fs.writeFileSync(fileKaPath,"");
+let path = require('path');
+let fs = require('fs');
+let arr = ['audio','video','image','software','documents','applications','other'];
+let extension  = ['.mp3','.mp4','.png','.exe','.doc','.zip','.pdf'];
+// console.log(__dirname);
+for(let folderName of arr){
+    let folderPath = path.join(__dirname,folderName);
+    if(!fs.existsSync(folderPath)){
+        fs.mkdirSync(folderPath);
     }
 }
+
