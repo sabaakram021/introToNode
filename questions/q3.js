e9a7e518452a11a47a99a03af14ac30b579c233c
saba akram <sabaakram211@gmail.com>
2023-04-01 12:32:20 +0530

    question 3 solved

--- questions/q3.js
+++ questions/q3.js
@@ -1,1 +1,14 @@
 //Q. copy index.html file from module1 to a new folder inside module2 having name html.
+// apne file k hisab se modify kiye hai file ko
+let path = require('path');
+let fs = require('fs');
+// console.log(__dirname);
+
+let sourcePath = path.join(__dirname,'..','unorganised','newd.doc');
+console.log(sourcePath);
+
+let destinationPath = path.join(__dirname,'organize','documents','newd.doc');
+console.log(destinationPath);
+
+fs.copyFileSync(sourcePath,destinationPath);
+
